91dea5e326a62adf6b1501322be47ba93e77be7c
danielcasto <danielcasto@ufl.edu>
2021-11-04 18:07:16 -0400

    Made data viewing simpler

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 from os import access
+import json
 import pyaudio
 import speech_recognition as speech
 from wit import Wit
 import sys
 
+
+def get_intents(response):
+	intents = []
+
+	for intent in response['intents']:
+		intents.append(intent['name'])
+		
+	return intents
+
+
+def get_entities_values(response):
+	entities_values = []
+
+	for entity in response['entities']:
+		for item in response['entities'][entity]:
+			entities_values.append(item['value'])
+
+	return entities_values
+
+
 def main():
+
+	if len(sys.argv) != 2:
+		print('Error: you entered the wrong amount of sys args')
+		exit(1)
+	
 	recognizer = speech.Recognizer()
 	audio = None
 	
@@ -18,8 +44,11 @@ def main():
 	google_understanding = recognizer.recognize_google(audio)
 	response = client.message(google_understanding)
 
-	print('Google heard: ', google_understanding)
-	print('Wit\'s resopnse: ', str(response))
+	print('Google heard: ', google_understanding, '\n')
+	print('Wit\'s resopnse: ', response)
+	print('Intent(s): ', get_intents(response))
+	print('Entities(s): ', get_entities_values(response))
+
 
 if __name__ == '__main__':
 	main()
