5160f2b2ded73749c5062417caa81ccde81a53f9
danielcasto <danielcasto@ufl.edu>
2021-11-04 16:21:50 -0400

    Made it adjust for ambient noise. Also the output is more clear.

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ def main():
 	
 	with speech.Microphone() as mic:
 		print('Speak now')
+		recognizer.adjust_for_ambient_noise(mic)
 		audio = recognizer.listen(mic)
 
 	ACCESS_CODE = sys.argv[1]
 	client = Wit(ACCESS_CODE)
-	response = client.message(recognizer.recognize_google(audio))
-	print(str(response))
+	google_understanding = recognizer.recognize_google(audio)
+	response = client.message(google_understanding)
+
+	print('Google heard: ', google_understanding)
+	print('Wit\'s resopnse: ', str(response))
 
 if __name__ == '__main__':
 	main()
