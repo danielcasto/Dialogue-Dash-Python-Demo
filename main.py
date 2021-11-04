7a0882b4b4661b27be7a13f51d99135538fe7342
danielcasto <danielcasto@ufl.edu>
2021-11-03 21:25:49 -0400

    Fixed listening

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from os import access
 import pyaudio
 import speech_recognition as speech
 from wit import Wit
@@ -5,16 +6,15 @@ import sys
 
 def main():
 	recognizer = speech.Recognizer()
-	audio = ''
+	audio = None
 	
 	with speech.Microphone() as mic:
 		print('Speak now')
 		audio = recognizer.listen(mic)
 
 	ACCESS_CODE = sys.argv[1]
-
 	client = Wit(ACCESS_CODE)
-	response = client.message(audio)
+	response = client.message(recognizer.recognize_google(audio))
 	print(str(response))
 
 if __name__ == '__main__':
